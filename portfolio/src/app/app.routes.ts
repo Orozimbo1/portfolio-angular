a612ea7d379528c9626bb251b4ebac31cba16158
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-03-25 20:19:58 -0300

    Passos iniciais do projeto

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+// Components Pages
+import { HomeComponent } from './modules/portfolio/pages/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
