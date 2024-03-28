d801e645a71c59d206326bbb1b7d6eab60f07689
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2024-03-28 20:00:00 -0300

    Projects Components HTML

--- portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 // Components
 import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgesComponent } from '../../components/knowledges/knowledges.component';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     KnowledgesComponent,
-    ExperiencesComponent
+    ExperiencesComponent,
+    ProjectsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
